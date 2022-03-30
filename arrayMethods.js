946b0f8310bd1e11765105c66c1c9da97c9cd3e6
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-30 15:40:16 -0600

    array methods complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -18,7 +18,8 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+const evenNumbers = mixedNumbers.filter(element => element % 2 === 0);
+// console.log(evenNumbers);
 
 
 
@@ -39,8 +40,8 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
+const postTaxPrices = prices.map(price => price * (1.07));
+// console.log(postTaxPrices);
 
 
 ////////// PROBLEM 3 //////////
@@ -57,8 +58,8 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
+const totalPopulation  = populations.reduce((acc, population) => acc + population);
+// console.log(totalPopulation);
 
 
 ////////// PROBLEM 4 //////////
@@ -82,8 +83,8 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
+const myStrongest = monstersInYourPocket.filter((monst, i) => monst.CP > 200);
+// console.log(myStrongest);
 
 
 ////////// PROBLEM 5 //////////
@@ -100,8 +101,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 */
 
 // CODE HERE
-
-
+const mappedOrders = orders.map((elem,i) => {
+  return elem.price * (1 + elem.tax);
+})
+// console.log(mappedOrders);
 
 ////////// PROBLEM 6 //////////
 
@@ -120,3 +123,5 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 */
 
 // CODE HERE
+const bobsTotal = purchases.filter((purch) => purch.owner === "Bob").reduce((acc, purch) => acc + purch.price, 0)
+console.log(bobsTotal);
